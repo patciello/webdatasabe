bd7169e55c6a01a5656f5a719b6d7314ea3d4380
Leonardo Paciello Ferreira Dias <leonardopaciellop@gmail.com>
2024-09-26 05:48:39 -0300

    Change to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html", // Caminho para seus templates Flask
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
